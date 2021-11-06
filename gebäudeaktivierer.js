7245346f58e7968d2b0cf196948970ff0096910a
HerrWaldgott <39630187+HerrWaldgott@users.noreply.github.com>
2021-11-07 00:59:42 +0100

    gebäudeaktivierer hochgeladen

---
+++ gebäudeaktivierer.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Gebäudeaktivierer
+// @version      1.0.0
+// @author       HerrWaldgott
+// @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com)\/$/
+// @grant        none
+// ==/UserScript==
+
+(function () {
+	'use strict';
+	
+	alert('Hello World');
+})();
