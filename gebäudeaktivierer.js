1441953b509747ba522e5cf60593c2741a914ec0
HerrWaldgott <39630187+HerrWaldgott@users.noreply.github.com>
2021-11-07 01:02:28 +0100

    Update gebäudeaktivierer.js

--- gebäudeaktivierer.js
+++ gebäudeaktivierer.js
@@ -1,9 +1,11 @@
 // ==UserScript==
-// @name         Gebäudeaktivierer
+// @name         GebäudeAktivierer
 // @version      1.0.0
+// @description  Gebäudetypen auf einem Klick aktiveren oder deaktiveren
 // @author       HerrWaldgott
-// @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com)\/$/
+// @include      *://www.leitstellenspiel.de/
 // @grant        none
+// @namespace    https://github.com/HerrWaldgott/LSS-Scripte/raw/main/geb%C3%A4udeaktivierer.js
 // ==/UserScript==
 
 (function () {
