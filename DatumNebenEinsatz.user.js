bf9ac02bbb3ab95f2c50ded0415434006f7acf5e
HerrWaldgott <39630187+HerrWaldgott@users.noreply.github.com>
2021-11-15 21:48:07 +0100

    Update DatumNebenEinsatz.user.js

--- DatumNebenEinsatz.user.js
+++ DatumNebenEinsatz.user.js
@@ -11,33 +11,30 @@
 (async function() {
     'use strict';
     
-    function sleep(delay) {
-        var start = new Date().getTime();
-        while (new Date().getTime() < start + delay);
-    }
-    
     $('#mission_list_sicherheitswache > .missionSideBarEntry, #mission_list_krankentransporte > .missionSideBarEntry, #mission_list > .missionSideBarEntry').each(function(){
         var $this = $(this);
         var $attributes = $this[0].attributes;
         var missionId = +($attributes).mission_id.value;
-        $.get("https://www.leitstellenspiel.de/missions/" + missionId, function (data, status) {
-            var parser = new DOMParser();
-            var htmlDoc = parser.parseFromString(data, 'text/html');
-            var div = htmlDoc.getElementById('mission_general_info');
-            var date = div.getAttribute('data-generation-time');
-            const generateDate = new Date(date);
-            var dd = String(generateDate.getDate()).padStart(2, '0');
-            var mm = String(generateDate.getMonth() + 1).padStart(2, '0'); //January is 0!
-            var yyyy = generateDate.getFullYear();
-            var hour = String(generateDate.getHours()).padStart(2, "0");
-            var minute = String(generateDate.getMinutes()).padStart(2, "0");
-            var second = String(generateDate.getSeconds()).padStart(2, "0");
+        await new Promise(resolve => {
+          $.get("https://www.leitstellenspiel.de/missions/" + missionId, function (data, status) {
+                var parser = new DOMParser();
+                var htmlDoc = parser.parseFromString(data, 'text/html');
+                var div = htmlDoc.getElementById('mission_general_info');
+                var date = div.getAttribute('data-generation-time');
+                const generateDate = new Date(date);
+                var dd = String(generateDate.getDate()).padStart(2, '0');
+                var mm = String(generateDate.getMonth() + 1).padStart(2, '0'); //January is 0!
+                var yyyy = generateDate.getFullYear();
+                var hour = String(generateDate.getHours()).padStart(2, "0");
+                var minute = String(generateDate.getMinutes()).padStart(2, "0");
+                var second = String(generateDate.getSeconds()).padStart(2, "0");
 
-            var d = dd + '.' + mm + '.' + yyyy + " - " + hour + ":" + minute + ":" + second;
+                var d = dd + '.' + mm + '.' + yyyy + " - " + hour + ":" + minute + ":" + second;
 
-            var id = "mission_panel_heading_" + missionId;
-            $('#' + id).append("<small> (" + d + ")</span>");
+                var id = "mission_panel_heading_" + missionId;
+                $('#' + id).append("<small> (" + d + ")</span>");
+                window.setTimeout(resolve, 100);
+            });
         });
-        sleep(200);
     });
 })();
