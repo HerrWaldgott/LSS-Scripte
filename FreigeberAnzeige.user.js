5906587c81688fdae791ae1aad68a9528399f031
HerrWaldgott <39630187+HerrWaldgott@users.noreply.github.com>
2021-11-15 21:59:49 +0100

    Update FreigeberAnzeige.user.js

--- FreigeberAnzeige.user.js
+++ FreigeberAnzeige.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         FreigeberAnzeige
-// @version      1.0.0
+// @version      1.0.1
 // @description  Zeigt an wer den Einsatz freigegeben hat, Suchfunktion sucht dann auch nach Namen
 // @author       HerrWaldgott
 // @include      *://www.leitstellenspiel.de/
@@ -10,20 +10,23 @@
 (function() {
     'use strict';
 
-    $('#mission_list_alliance > .missionSideBarEntry, #mission_list_alliance_event > .missionSideBarEntry').each(function(){
+    $('#mission_list_alliance > .missionSideBarEntry, #mission_list_alliance_event > .missionSideBarEntry').each(async function(){
         var $this = $(this);
         var $attributes = $this[0].attributes;
         var missionId = +($attributes).mission_id.value;
-        $.get("https://www.leitstellenspiel.de/missions/" + missionId, function (data, status) {
-            var parser = new DOMParser();
-            var htmlDoc = parser.parseFromString(data, 'text/html');
-            var div = htmlDoc.getElementById('col_left');
-            var nameDiv = div.getElementsByClassName('alert-info');
-            var name = nameDiv[0].childNodes[1].innerHTML;
+        await new Promise(resolve => {
+            $.get("https://www.leitstellenspiel.de/missions/" + missionId, function (data, status) {
+                var parser = new DOMParser();
+                var htmlDoc = parser.parseFromString(data, 'text/html');
+                var div = htmlDoc.getElementById('col_left');
+                var nameDiv = div.getElementsByClassName('alert-info');
+                var name = nameDiv[0].childNodes[1].innerHTML;
 
-            var id = "mission_address_" + missionId;
-            $('#' + id).text($('#' + id).text() + " - " + name);
-            $('#mission_' + missionId).attr('search_attribute', $('#mission_' + missionId).attr('search_attribute') + ", " + name);
+                var id = "mission_address_" + missionId;
+                $('#' + id).text($('#' + id).text() + " - " + name);
+                $('#mission_' + missionId).attr('search_attribute', $('#mission_' + missionId).attr('search_attribute') + ", " + name);
+                window.setTimeout(resolve, 100);
+            });
         });
     });
 })();
