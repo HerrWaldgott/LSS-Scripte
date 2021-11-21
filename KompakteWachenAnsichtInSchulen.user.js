76bc843a5b9235ac62f7a5e58c72c4c40e2b82a2
HerrWaldgott <39630187+HerrWaldgott@users.noreply.github.com>
2021-11-21 02:42:49 +0100

    Update KompakteWachenAnsichtInSchulen.user.js

--- KompakteWachenAnsichtInSchulen.user.js
+++ KompakteWachenAnsichtInSchulen.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         KompakteWachenAnsichtInSchulen
-// @version      1.0.0
+// @version      1.1.0
 // @description  Gegliederte Ansicht der Wachen in einer Schule
 // @author       HerrWaldgott
 // @include      *://www.leitstellenspiel.de/buildings/*
@@ -23,18 +23,18 @@
 
     if ((building.building_type == 1 || building.building_type == 3 || building.building_type == 8 || building.building_type == 10) && window.location.href == "https://www.leitstellenspiel.de/buildings/" + buildingID){
         $('#accordion').after(`
-            <div>
+            <div id="tabsDiv">
                 <ul id="tabsBuilding" class="nav nav-tabs" role="tablist">
-                    <li role="presentation">
+                    <li role="presentation" id="feuerwehrTab">
                         <a href="#feuerwehr" aria-controls="feuerwehr" role="tab" data-toggle="tab">Feuerwehr</a>
                     </li>
-                    <li role="presentation" class="">
+                    <li role="presentation" class="" id="rettungsdienstTab">
                         <a href="#rettungsdienst" aria-controls="rettungsdienst" role="tab" data-toggle="tab">Rettungsdienst</a>
                     </li>
-                    <li role="presentation" class="">
+                    <li role="presentation" class="" id="polizeiTab">
                         <a href="#polizei" aria-controls="polizei" role="tab" data-toggle="tab">Polizei</a>
                     </li>
-                    <li role="presentation" class="">
+                    <li role="presentation" class="" id="thwTab">
                         <a href="#thw" aria-controls="thw" role="tab" data-toggle="tab">THW</a>
                     </li>
                 </ul>
@@ -42,19 +42,19 @@
                     <div role="tabpanel" class="tab-pane" id="feuerwehr"></div>
                     <div role="tabpanel" class="tab-pane" id="rettungsdienst">
                         <ul id="tabsBuilding" class="nav nav-tabs" role="tablist">
-                            <li role="presentation">
+                            <li role="presentation" id="rdTab">
                                 <a href="#rd" aria-controls="rd" role="tab" data-toggle="tab">Rettungswache</a>
                             </li>
-                            <li role="presentation" class="">
+                            <li role="presentation" class="" id="rd_rhsTab">
                                 <a href="#rd_rhs" aria-controls="rd_rhs" role="tab" data-toggle="tab">Rettungshundestaffel</a>
                             </li>
-                            <li role="presentation" class="">
+                            <li role="presentation" class="" id="rd_segTab">
                                 <a href="#rd_seg" aria-controls="rd_seg" role="tab" data-toggle="tab">Schnelleinsatzgruppe</a>
                             </li>
-                            <li role="presentation" class="">
+                            <li role="presentation" class="" id="rd_rthTab">
                                 <a href="#rd_rth" aria-controls="rd_rth" role="tab" data-toggle="tab">Rettungshelikopter</a>
                             </li>
-                            <li role="presentation" class="">
+                            <li role="presentation" class="" id="rd_wrTab">
                                 <a href="#rd_wr" aria-controls="rd_wr" role="tab" data-toggle="tab">Wasserrettung</a>
                             </li>
                         </ul>
@@ -68,16 +68,16 @@
                     </div>
                     <div role="tabpanel" class="tab-pane" id="polizei">
                         <ul id="tabsBuilding" class="nav nav-tabs" role="tablist">
-                            <li role="presentation">
+                            <li role="presentation" id="polTab">
                                 <a href="#pol" aria-controls="pol" role="tab" data-toggle="tab">Polizeiwache</a>
                             </li>
-                            <li role="presentation" class="">
+                            <li role="presentation" class="" id="pol_seTab">
                                 <a href="#pol_se" aria-controls="pol_se" role="tab" data-toggle="tab">Sondereinheiten</a>
                             </li>
-                            <li role="presentation" class="">
+                            <li role="presentation" class="" id="pol_pthTab">
                                 <a href="#pol_pth" aria-controls="pol_pth" role="tab" data-toggle="tab">Polizeihubschrauber</a>
                             </li>
-                            <li role="presentation" class="">
+                            <li role="presentation" class="" id="pol_bpTab">
                                 <a href="#pol_bp" aria-controls="pol_bp" role="tab" data-toggle="tab">Bereitschaftspolizei</a>
                             </li>
                         </ul>
@@ -97,7 +97,6 @@
             var $panel = $(this);
             var buildingID = $($panel.find("div.panel-heading")[0]).attr("building_id");
             var building = cBuildings.filter(b => b.id == buildingID)[0];
-            console.log(building);
             switch(building.building_type){
                 case 0:
                 case 18:
@@ -140,5 +139,20 @@
                     break;
             }
         });
+
+        $('#tabsDiv').find('div.tab-pane').each(function() {
+            var $pane = $(this);
+            if ($pane.text() == ""){
+                $('#' + $pane.attr("id") + "Tab").addClass("hidden");
+            }
+        });
+
+        if ($('#rettungsdienst > ul > li:not(.hidden)').length == 0){
+            $('#rettungsdienstTab').addClass("hidden");
+        }
+
+        if ($('#polizei > ul > li:not(.hidden)').length == 0){
+            $('#polizeiTab').addClass("hidden");
+        }
     }
 })();
